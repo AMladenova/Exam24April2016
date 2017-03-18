b9523a9f6099c436d17a9b4323764b8829e1c7e4
AMladenova <mladenova.anna@gmail.com>
2017-03-18 08:59:43 +0200

    Adding new problem.

--- SpecialNumbers.java
+++ SpecialNumbers.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 /**
  * Created by mlade on 16/03/2017.
  */
 public class SpecialNumbers {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        int n = Integer.parseInt(console.nextLine());
+
+        for (int a = 1; a < 10; a++) {
+            for (int b = 1; b < 10; b++) {
+                for (int c = 1; c < 10; c++) {
+                    for (int d = 1; d < 10; d++) {
+                        if ((n % a == 0) && (n % b == 0) && (n % c == 0) && (n % d == 0)) {
+                            System.out.printf("%d%d%d%d ",a,b,c,d);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- StopSign.java
+++ StopSign.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 /**
  * Created by mlade on 16/03/2017.
  */
 public class StopSign {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        int n = Integer.parseInt(console.nextLine());
+
+        int dots = n + 1;
+        int dashes = 2 * n + 1;
+
+        System.out.println(repeatStr(".", n + 1) + repeatStr("_", 2 * n + 1) + repeatStr(".", n + 1));
+        dashes -= 2;
+        for (int i = 0; i < n; i++) {
+            System.out.println(repeatStr(".",dots - 1) + "//" + repeatStr("_", dashes) + "\\\\" + repeatStr(".",dots - 1));
+            dots --;
+            dashes += 2;
+        }
+        System.out.println("//" + repeatStr("_", (dashes - 5) / 2) +"STOP!"+ repeatStr("_", (dashes - 5) / 2) + "\\\\");
+        for (int i = 0; i < n; i++) {
+            System.out.println(repeatStr(".",dots - 1) + "\\\\" + repeatStr("_", dashes) + "//" + repeatStr(".",dots - 1));
+            dots ++;
+            dashes -= 2;
+        }
+    }
+
+    static String repeatStr(String strToRepeat, int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(strToRepeat);
+        }
+        return sb.toString();
+    }
 }
